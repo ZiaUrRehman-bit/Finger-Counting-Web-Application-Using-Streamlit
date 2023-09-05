39a617573fec118a0c1806448916405572a389e2
hp <engrziaurrehman.kicsit@gmail.com>
2023-09-05 14:23:47 +0500

    Updated

--- app/main.py
+++ app/main.py
@@ -1,1 +1,47 @@
-print("Hello World!")
+import cv2
+import mediapipe as mp
+import streamlit as st
+import tempfile
+import time
+import PoseModule as pm
+
+cam = cv2.VideoCapture(0)
+
+st.title("Video Capture")
+
+framePlaceholder = st.empty()
+
+
+cTime = 0
+pTime = 0
+detector = pm.poseDetector()
+while True:
+
+    Success, frame = cam.read()
+    frame = detector.findPose(frame)
+    lm = detector.findPosLm(frame)
+
+    print(lm[0])
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(frame, str(int(fps)), (20, 70),
+                cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3)
+
+    # cv2.imshow("Pose Estimation", frame)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    framePlaceholder.image(frame, channels="RGB")
+    
+    key = cv2.waitKey(1)
+
+    if key == 81 or key == 113:
+        break
+
+
+cam.release()
+cv2.destroyAllWindows()
+
+print("Code Completed!")
